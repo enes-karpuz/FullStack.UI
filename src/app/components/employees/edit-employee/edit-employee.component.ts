2d06a27e371f2f154e2249ee045a6a918094f51f
Enes Karpuz <eneskarpuz@gmail.com>
2023-05-30 20:43:57 +0300

    Kontroller Tamamlandı

--- src/app/components/employees/edit-employee/edit-employee.component.ts
+++ src/app/components/employees/edit-employee/edit-employee.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Route } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EmployeesService } from 'src/app/services/employees.service';
+import { Employee } from 'src/app/models/employee.model';
 
 @Component({
   selector: 'app-edit-employee',
@@ -7,15 +9,41 @@ import { ActivatedRoute, Route } from '@angular/router';
   styleUrls: ['./edit-employee.component.css']
 })
 export class EditEmployeeComponent implements OnInit {
-  constructor(private route: ActivatedRoute) { }
+  employeeDetails : Employee = {
+    id : '',
+    name : '',
+    email : '',
+    phone : 0,
+    salary : 0,
+    department : ''
+  };
+  constructor(private route: ActivatedRoute , private employeeService : EmployeesService, private router : Router) { }
   ngOnInit(): void {
     this.route.paramMap.subscribe({
       next: (params) => {
         const id = params.get('id');
         if (id) {
-          
+          this.employeeService.getEmployee(id).subscribe({
+            next : (response) => {
+              this.employeeDetails = response;
+            }
+          })
         }
       }
     })
+  }
+  updateEmployee(){
+    this.employeeService.updateEmployee(this.employeeDetails.id , this.employeeDetails).subscribe({
+      next : (response) => {
+        this.router.navigate(['employees']); //sayfayı employees'e geri döndürür.
+      }
+    });
+  }
+  deleteEmployee(id : string){
+    this.employeeService.deleteEmployee(id).subscribe({
+      next : (response) => {
+        this.router.navigate(['employee']);
+      }
+    });
   }
 }
--- src/app/services/employees.service.ts
+++ src/app/services/employees.service.ts
@@ -20,7 +20,14 @@ export class EmployeesService {
     return this.http.post<Employee>(this.baseApiUrl , addEmployeeRequest);
   }
 
-  getEmployee(id : string): Observable<Employee[]>{
-   return this.http.get<Employee[]>(this.baseApiUrl , id);
+  getEmployee(id : string): Observable<Employee>{
+   return this.http.get<Employee>(this.baseApiUrl + id);
+  }
+
+  updateEmployee(id : string , updateEmployeeRequest : Employee) : Observable<Employee>{
+   return this.http.put<Employee>(this.baseApiUrl + id, updateEmployeeRequest);
+  }
+  deleteEmployee(id : string) : Observable<Employee>{
+    return this.http.delete<Employee>(this.baseApiUrl + id);
   }
 }
